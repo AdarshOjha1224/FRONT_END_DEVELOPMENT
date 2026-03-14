ef8350294bbe3de0312302991a8f2feffed53bfb
AdarshOjha1224 <adarshojha1224@gmail.com>
2026-03-14 23:46:03 +0530

    Immediately Invoked Function Expressions in JS

---
+++ JAVASCRIPT/IIFE.js
@@ -0,0 +1,21 @@
+// Immediately Invoked Function Expressions (IIFE)
+// need - To stop creating problem in the function because of global scope.
+//      - immediately Execution.
+
+// (function)()
+
+(function first() {
+    console.log(`DB CONNECTED`)
+})();
+
+// ; -> to stop/end the Invoked function.
+
+(function second() {  // Named IIFE
+    console.log(`DB CONNECTED TWO`)
+})();
+
+
+( (name) => {  // Arrow function (unnamed IIFE)
+    console.log(`DB CONNECTED THREE ${name}`)
+})("Adarsh")
+
