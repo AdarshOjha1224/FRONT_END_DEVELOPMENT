1c1179a9a0ef2ce984688e949c13fe40b092a362
AdarshOjha1224 <adarshojha1224@gmail.com>
2026-03-16 20:41:23 +0530

    Switch in flowControl in javascript.

---
+++ JAVASCRIPT/23_Switch.js
@@ -0,0 +1,33 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// DataType of key and value should be same.
+
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("February");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    default:
+        console.log("Unvalid Input");
+        break;
+}
