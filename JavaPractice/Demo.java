c8684708d0aedb9ad80b4bf2a906a8de30ed0223
AdarshOjha1224 <adarshojha1224@gmail.com>
2026-03-25 22:54:30 +0530

    Committed

--- JavaPractice/Demo.java
+++ JavaPractice/Demo.java
@@ -1,15 +1,7 @@
 package JavaPractice;
 
-class Student {
-    
-}
-
-
 public class Demo {
-    public static void main(String[] args) {
-        String name = new String();
-        // Integer age = new Integer();
-        
+    public static void main(String[] args){
         System.out.println("This is just to maintain the Github Graph");
     }
 }
